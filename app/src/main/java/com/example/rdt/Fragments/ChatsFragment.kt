c4db1c94f58726973f29e7ffddb3dd30acf7df31
Abu Hejleh <abood.kamal.abuhejleh@gmail.com>
2020-12-27 14:23:14 +0200

    there is a small problem

--- app/src/main/java/com/example/rdt/Fragments/ChatsFragment.kt
+++ app/src/main/java/com/example/rdt/Fragments/ChatsFragment.kt
@@ -5,17 +5,96 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.rdt.Needed.Chatlist
+import com.example.rdt.Needed.User
 import com.example.rdt.R
+import com.example.rdt.adapters.UserAdapter
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.fragment_chats.*
 
 class ChatsFragment : Fragment() {
+
+
+private lateinit var userAdapter: UserAdapter
+private lateinit var mUser :ArrayList<User>
+    private lateinit var fUser :FirebaseUser
+    private lateinit var dbRef :DatabaseReference
+    private lateinit var usersList : ArrayList<Chatlist>
+
+lateinit var recyclerView : RecyclerView
+
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view :View =inflater.inflate(R.layout.fragment_chats, container, false)
+        recyclerView= view.findViewById(R.id.chat_recycler_view)
 
-        return inflater.inflate(R.layout.fragment_chats, container, false)
+
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        fUser = FirebaseAuth.getInstance().currentUser!!
+        usersList = ArrayList()
+        dbRef = FirebaseDatabase.getInstance().getReference("ChatList").child(fUser.uid)
+        val listener = object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+            usersList.clear()
+                var chatlist :Chatlist
+                for (snap :DataSnapshot in snapshot.children){
+                    chatlist =snap.getValue(Chatlist::class.java)!!
+                    usersList.add(chatlist)
+
+            }
+                chatList()
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        }
+        dbRef.addValueEventListener(listener)
+
+
+        return view
+    }
+
+    private fun chatList() {
+        mUser = ArrayList()
+        dbRef = FirebaseDatabase.getInstance().getReference("Users")
+        var user :User
+        val listen = object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+              mUser.clear()
+                for (snap :DataSnapshot in snapshot.children){
+
+               user= snap.getValue(User::class.java)!!
+                for (chatList:Chatlist in usersList){
+                    if (user.getId().equals(chatList.getId())){
+                        mUser.add(user)
+                    }
+
+                }
+                }
+
+            userAdapter = UserAdapter(context!! , mUser)
+                recyclerView.adapter = userAdapter
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        }
+        dbRef.addValueEventListener(listen)
     }
 
 
